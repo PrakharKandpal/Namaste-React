f137b1c08a3c8ec570e9022e99dc68b89b382f92
Prakhar Kandpal <prakhar13kandpal@gmail.com>
2024-09-04 21:55:09 +0530

    Initial commit with app.js and other files

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const parent = React.createElement( "div", {id: "parent"}, [
-    React.createElement("div", {id: "child"},
-        [React.createElement("h1",{},"I am an h1 tag"),
-         React.createElement("h2",{},"I am an h2 tag"),
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child" },
+      [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")
+      ]
+    ),
+    React.createElement(
+      "div",
+      { id: "child" },
+      [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")
+      ]
+    )
+  ]
+);
 
-        ]),
-    
-        React.createElement("div", {id: "child"},
-            [React.createElement("h1",{},"I am an h1 tag"),
-             React.createElement("h2",{},"I am an h2 tag"),
-    
-            ]),
-    ]);
-    
-    
-  console.log(parent);
+const jsxHeading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React jsx
+  </h1>
+);
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-    
+
